8ed9cc63908642b482048643d8cb033152e5452d
riassuc <riassuc@gmail.com>
2021-05-24 18:26:24 +0900

    add: urls.py, views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
+from django.views import View
+from django.http import JsonResponse
+from .models import Menu
 
-# Create your views here.
+class ProductListView(View):
+    def get(self, request):
+        menus = Menu.objects.all()
+        result = []
+        for menu in menus:
+            result.append(menu.name)
+        return JsonResponse({'result': result}, status=200)
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body) #json->python
+            Menu.objects.create(name = data['name'])
+            return JsonResponse({'result': "SUCCESS"}, status=201)
+        except KeyError:
+            return JsonResponse({'message': 'INVAILID_KEY'}, status=400)
